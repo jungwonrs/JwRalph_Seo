32fc1cbc3e398291b0980741526ad66a5f91f5c4
Jungwon Seo <jungwonrs@gmail.com>
2017-05-18 15:16:14 +0900

    Parcelable
    Parcelable

---
+++ Android/4 - Parcelable(example)/app/src/main/java/com/example/jungwonseo/parcelable/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.jungwonseo.parcelable;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String KEY_SIMPLE_DATA = "data";
+    public static final int REQUEST_CODE_MENU = 101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void onButton1Clicked(View v) {
+        Intent intent = new Intent(getApplicationContext(), Menu.class);
+
+        SimpleData data = new SimpleData(100, "Hello Android!");
+        intent.putExtra(KEY_SIMPLE_DATA, data);
+
+        startActivityForResult(intent, REQUEST_CODE_MENU);
+
+    }
+}
